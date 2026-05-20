b850bb0549a0e927e372718db7c1ee807ca19d45
Amaan <amaanali@outlook.fr>
2026-05-20 21:47:40 +0200

    Ajout des fichiers manquant pour LA CI/ CD

---
+++ src/main/java/fr/ensitech/myproject/entity/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package fr.ensitech.myproject.entity.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
